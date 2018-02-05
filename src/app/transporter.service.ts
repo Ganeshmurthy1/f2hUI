7f00f1e7abebbda0d25ac20b57c73e434c121e5a
Ganeshmurthy <ganeshmurthy.p@gmail.com>
2018-02-05 18:44:19 +0530

    test

--- src/app/transporter.service.ts
+++ src/app/transporter.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
-
+import { Http, Response,URLSearchParams,Headers,RequestOptions} from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import { CommonService } from './common.service';
 @Injectable()
-export class TransporterService {
+export class TransporterService extends CommonService{
 
-  constructor() { }
+  constructor(private http:Http) {
+    super();
+    console.log(this.url);
+ }
 
 }
